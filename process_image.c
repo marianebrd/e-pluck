ad81a33ff59c65010b5a9257022ee5b3d989a9cc
Mariane Brodier <mariane.brodier@gmail.com>
2020-05-08 13:06:19 +0200

    update RGB to HSV

--- process_image.c
+++ process_image.c
@@ -56,8 +56,9 @@ static THD_FUNCTION(ProcessImage, arg) {
 		//gets the pointer to the array filled with the last image in RGB565
 		img_buff_ptr = dcmi_get_last_image_ptr();
 
-		volatile int debug1, debug2;
+		//volatile int debug1, debug2;
 
+		/*
 		//Extracts RGB pixels
 		for(uint16_t i = 0 ; i < (2 * IMAGE_BUFFER_SIZE) ; i++){
 			if(i%2 == 0){
@@ -71,15 +72,34 @@ static THD_FUNCTION(ProcessImage, arg) {
 				buffer2[(i-1)/2] = ((uint8_t)img_buff_ptr[i]&0xE0) >> 5; //green mask on second line
 			}
 		}
-		debug1 = img_buff_ptr[320];
-		debug2 = img_buff_ptr[321];
+		*/
+
+		//Extracts RGB pixels
+		for(uint16_t i = 0 ; i < (2 * IMAGE_BUFFER_SIZE) ; i++){
+			if(i%2 == 0){
+				image_b[i/2] = (((uint8_t)img_buff_ptr[i]&0xF8) >> 3)*255 / 31; //red mask
+				//debug1 = image_r[i/2];
+				buffer1[i/2] = ((uint8_t)img_buff_ptr[i]&0x07); //green mask on first line
+			}
+			else{
+				image_r[(i-1)/2] = ((uint8_t)img_buff_ptr[i]&0x1F)*255 / 31; //blue mask
+				//debug2 = image_b[(i-1)/2];
+				buffer2[(i-1)/2] = ((uint8_t)img_buff_ptr[i]&0xE0) >> 2; //green mask on second line
+			}
+		}
+
+		//debug1 = img_buff_ptr[320];
+		//debug2 = img_buff_ptr[321];
+		//debug1 = buffer1[320];
+		//debug2 = buffer2[320];
 
 		//converts the rgb values to hsv
-		for (uint16_t i = 0 ; i < IMAGE_BUFFER_SIZE ; i++){
+		for (uint16_t i = 0 ; i < (2 * IMAGE_BUFFER_SIZE) ; i+=2){
 			image_g[i] = ((buffer1[i] | buffer2[i]))*255 / 63; //conversion 6 bits on 5 bits
 			if(i > 315)
 				image[i] = rgb_to_hsv(image_r[i], image_g[i], image_b[i]);
 		}
+
 		//chThdSleepMilliseconds(100);
     }
 }
@@ -87,9 +107,9 @@ static THD_FUNCTION(ProcessImage, arg) {
 uint8_t rgb_to_hsv(uint8_t r, uint8_t g, uint8_t b){
 	// RGB values are divided by 255
 	// to change the range from 0..255 to 0..1:
-	uint8_t h = 0;
-	uint8_t s = 0;
-	//uint8_t v = 0;
+	volatile uint16_t h = 0;
+	volatile uint8_t s = 0;
+	volatile uint8_t v = 0;
 
 	volatile uint8_t cmax = max_value(r,g,b);
 	volatile uint8_t cmin = min_value(r,g,b);
@@ -99,11 +119,11 @@ uint8_t rgb_to_hsv(uint8_t r, uint8_t g, uint8_t b){
 	if (cmax == cmin)
 	      h = 0;
 	else if (cmax == r)
-	      h = (60 * ((g - b) / diff) + 360) % 360;
+	      h = ((60 * ((g - b) * 100) / diff) + 360) % 360;
 	else if (cmax == g)
-	      h = (60 * ((b - r) / diff) + 120) % 360;
+	      h = ((60 * ((b - r) * 100) / diff) + 120) % 360;
 	else if (cmax == b)
-	      h = (60 * ((r - g) / diff) + 240) % 360;
+	      h = ((60 * ((r - g) * 100) / diff) + 240) % 360;
 
 	// compute s (range from 0..100)
 	if (cmax == 0)
@@ -112,19 +132,18 @@ uint8_t rgb_to_hsv(uint8_t r, uint8_t g, uint8_t b){
 	      s = ((diff * 100) / cmax);
 
 	// compute v (range from 0..100)
-	//v = (cmax * 100) / 255;
+	v = (cmax * 100) / 255;
 
 	static uint8_t color = 10;
 
-	if (((cmax * 100) / 255) > 15){
+	if (v > 15){
 		if (s > 15){
-			if ((h < 50) && (h > 300))
+			if ((h > 300) && (h < 50))
 				color = 0;
 			else if ((h > 50) && (h < 200))
 				color = 1;
 			else if ((h > 200) && (h < 300))
 				color = 2;
-			else color = 3;
 		}
 		else color = 4;
 	}
